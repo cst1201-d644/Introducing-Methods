e90a542410cc1cb8ce040ac67f6fd4b4255f293f
laboni <laboni@laboni-THINK.home>
2015-04-27 23:54:38 -0400

    finished exercise 1

--- IntroducingMethods/src/cst1201/IntroducingMethods.java
+++ IntroducingMethods/src/cst1201/IntroducingMethods.java
@@ -11,15 +11,13 @@ public class IntroducingMethods {
 
     public static void main(String[] args) {
 
-        System.out.print("Please enter a string: ");
+        System.out.print(" Please enter a string: ");
 
         Scanner keyboard = new Scanner(System.in);
         getInputString(keyboard);
+
     }
 
-    // Given a Scanner, prompt the user for a String.  If the user enters an empty
-    // String, report an error message and ask for a non-empty String.  Return the
-    // String to the calling program.
     private static String getInputString(Scanner inScanner) {
 
         String input = inScanner.nextLine();
@@ -27,26 +25,17 @@ public class IntroducingMethods {
         while (input.length() == 0) {
 
             System.out.println("ERROR - string must not be empty.");
-            System.out.println(" Please enter a string: ");
+            System.out.println("Enter a string: ");
 
             input = inScanner.nextLine();
 
             getWordCount(input);
-            getFirstWord(input);
 
         }
 
-        System.out.println("PLease indicate the nth word you would like: ");
-
-        int n = inScanner.nextInt();
-
         getWordCount(input);
-        getFirstWord(input);
-
-        getWord(input, n);
 
         return input;
-
     }
 
     private static int getWordCount(String input) {
@@ -59,25 +48,4 @@ public class IntroducingMethods {
 
     }
 
-    private static String getFirstWord(String input) {
-
-        if (input.indexOf(' ') > -1) {
-
-            System.out.println("The First Word is: " + input.substring(0, input.indexOf(' ')));
-            return input.substring(0, input.indexOf(' '));
-
-        } else {
-
-            System.out.println("The First Word is: " + input);
-            return input;
-        }
-
-    }
-
-    private static String getWord(String input, int n) {
-        String arr[] = input.split(" ");
-        String word = arr[n];
-        System.out.println("The word you were looking for:  " + word);
-        return word;
-    }
 }
